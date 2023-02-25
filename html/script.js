a8fd33e4d85a48e24286485c504a400a4fe5351d
imsamrat <imsamrat24@gmail.com>
2023-02-25 22:58:54 +0600

    Updated Version

--- html/script.js
+++ html/script.js
@@ -1,78 +1,158 @@
 //select dom element
-
-const counterEl = document.getElementById('counter');
-const incrementEl = document.getElementById('increment');
-const decrementEl = document.getElementById('decrement');
-
-const containerEl = document.getElementById('container');
-const matchEl = document.getElementById('addMatch');
-
+//  all-matches lws-addMatch lws-reset
+const matchContainer = document.querySelector(".all-matches");
+const addMatchBtn = document.querySelector(".lws-addMatch");
+const resetBtn = document.querySelector(".lws-reset");
 
 //action identifiers
-const INCREMENT = 'increment';
-const DECREMENT = 'decrement';
-
+const INCREMENT = "score/increment";
+const DECREMENT = "score/decrement";
+const RESET = "score/reset";
+const ADD_MATCH = "match/add";
+const DELETE_MATCH = "match/delete";
 
 // action creators
-const increment = (value) =>{
-  return{
+const increment = (payload) => {
+  return {
     type: INCREMENT, //mendatory
-    payload: value,
-  }
-}
-const decrement = (value) =>{
-  return{
+    payload,
+  };
+};
+const decrement = (payload) => {
+  return {
     type: DECREMENT,
-    payload: value,
-  }
-}
-
-
-const initialState = {
-  value: 0,
+    payload,
+  };
 };
 
-//create reducer function
-function counterReducer(state = initialState, action) {
-  if (action.type === INCREMENT) {
-    return {
-      ...state,
-      value: state.value + action.payload,
-    };
-  } else if (action.type === DECREMENT) {
-    return {
-      ...state,
-      value: state.value - action.payload,
-    };
-  } else {
+const reset = () => {
+  return {
+    type: RESET,
+  };
+};
+const addMatch = () => {
+  return {
+    type: ADD_MATCH,
+  };
+};
+
+const deleteMatch = (matchId) => {
+  return { type: DELETE_MATCH, payload: matchId };
+};
+
+const initialState = [
+  {
+    id: 1,
+    score: 0,
+  },
+];
+
+const nextMatchId = (matches) =>{
+  const maxId = matches.reduce((maxId, match) =>Math.max(match.id, maxId), -1);
+  return maxId + 1;
+}
+
+
+function matchReducer(state=initialState,action){
+  if(action.type === INCREMENT){
+    const newMatches = state.map((item)=>{
+      if(item.id === action.payload.id){
+        return{...item, score: item.score + Number(action.payload.value)};
+      }else{
+        return item;
+      }
+    })
+    return newMatches;
+  }
+  else if(action.type === DECREMENT){
+    const newMatches = state.map((item)=>{
+      if(item.id === action.payload.id){
+        const newScore = item.score - Number(action.payload.value);
+        return{...item, score: newScore > 0 ? newScore : 0 };
+      }else{
+        return item;
+      }
+    })
+    return newMatches;
+  }
+  else if (action.type === RESET){
+    const refreshedMatches = state.map((item)=>({...item, score:0}));
+    return refreshedMatches;
+  }
+  else if(action.type === ADD_MATCH){
+    const id = nextMatchId(state);
+    return [...state,{id,score:0}];
+  }
+  else if(action.type === DELETE_MATCH){
+    const newMatches = state.filter((match)=>match.id !== action.payload);
+    return newMatches;
+  }
+  else{
     return state;
   }
 }
 
 //create store
-const store = Redux.createStore(counterReducer);
+const store = Redux.createStore(matchReducer);
+
+
+const incrementHandler = (id,formEl) =>{
+  const input = formEl.querySelector('.incrementInput');
+  const value = Number(input.value);
+  if(value>0){
+    store.dispatch(increment({id,value}));
+  }
+}
+
+const decrementHandler = (id,formEl) =>{
+  const input = formEl.querySelector('.decrementInput');
+  const value = Number(input.value);
+  if(value>0){
+    store.dispatch(decrement({id,value}));
+  }
+}
+
+const handleMatchDelete = (matchId) =>{
+  store.dispatch(deleteMatch(matchId));
+}
+
+addMatchBtn.addEventListener('click',()=>{
+  store.dispatch(addMatch());
+})
+
+resetBtn.addEventListener('click',()=>{
+  store.dispatch(reset());
+})
 
 const render = () => {
-  const state = store.getState(); 
-  counterEl.innerText = state.value;
-}
+  const state=store.getState();
+  const MatchesView = state
+  .map((item)=>{
+    return `
+    <div class="match">
+    <div class="wrapper">
+    <button class="lws-delete" onclick="handleMatchDelete(${item.id})">
+    <img src="./image/delete.svg" alt="" /></button>
+    <h3 class="matchName">Match ${item.id}</h3>
+    </div>
+    <div class="inc-dec">
+    <form class="incrementForm" onsubmit="event.preventDefault(); incrementHandler(${item.id},this)">
+    <h4>Increment</h4>
+    <input type="number" name="increment" class="incrementInput"/>
+    </form>
+    <form class="decrementForm" onsubmit="event.preventDefault(); decrementHandler(${item.id},this)">
+    <h4>Decrement</h4>
+    <input type="number" name="decrement" class="decrementInput"/>
+    </form>
+    </div>
+    <div class="numbers">
+    <h2 class"singleResult">${item.score}</h2>
+    </div>
+    </div>`
+  })
+  .join("");
+  matchContainer.innerHTML = MatchesView;
+};
 render();
 store.subscribe(render);
 
-//button click listeners
-incrementEl.addEventListener("keypress", (e)=>{
-  // e.preventDefault();
-  store.dispatch(increment(e.target.value));
-})
-
-
-decrementEl.addEventListener("keypress", (e)=>{
-  // e.preventDefault();
-  store.dispatch(decrement(e.target.value));
-})
-
-document.getElementById("addMatch").onclick = function() {
-  var container = document.getElementById("container");
-  var section = document.getElementById("mainsection");
-  container.appendChild(section.cloneNode(true));
-}
